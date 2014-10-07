82ad7a3504e301608278b0865a6ea6eb662a5294
Siddharth Jain <sjain@guidewire.com>
2014-10-07 16:57:09 -0700

    Changes

--- src/sql_to_mongo.py
+++ src/sql_to_mongo.py
@@ -4,6 +4,7 @@
 
 import MySQLdb
 import pymongo
+import datetime
 
 ########################################################################################
 #	CONSTANTS
@@ -39,80 +40,86 @@ allSQLTableNames = ["circuits",
 ########################################################################################
 
 def main():
+	print("\n## STARTING PROGRAM ##\n")
+	
+	# Retrieve SQL Data
+	[sqlData,sqlColumns] = retrieveSQLData()
 	
 	# Connect to MongoDB
-	mongoClient = connectToMongo()
-	mongoDB = createNewMongoClient(mongoClient)
-
-	# Connect to SQL Database
-	#sqlCursor = connectToSQL()
-	
-	# Print all SQL Tables
-	#[sqlTableRowDict,sqlTableColDict] = printAllSQLTableData(sqlCursor)
+	mongoDB = connectToMongo()
+	storeInMongoTableStatuses(mongoDB,"status",sqlData,sqlColumns)
+	storeInMongoTableDrivers(mongoDB,"drivers",sqlData,sqlColumns)
 
 ########################################################################################
 #	SQL FUNCTIONS
 ########################################################################################
-
-def connectToSQL():
-	print("## CONNECTING TO SQL DATABASE ##\n")
+	
+def retrieveSQLData():
+	print("## CONNECTING TO SQL DATABASE %s ON HOST %s##\n"%(sqlDB,sqlHost))
 	db = MySQLdb.connect(host=sqlHost, user=sqlUser, passwd=sqlPassword, db=sqlDB)
 	cur = db.cursor()
-	return cur
-	
-def printAllSQLTableData(sqlCursor):
 	sqlTables = allSQLTableNames
-	sqlTableRowDict = {}
-	sqlTableColDict = {}
+	sqlData = {}
+	sqlColumns = {}
 	for sqlTable in sqlTables:
-		colCount = getSQLTableData(sqlCursor,sqlTable)
-		rowCount = printSQLTableData(sqlCursor,sqlTable,0,colCount,False)
-		sqlTableRowDict[sqlTable] = rowCount
-		sqlTableColDict[sqlTable] = colCount
-	return sqlTableRowDict,sqlTableColDict
+		data, columns = getSQLTableData(cur,sqlTable, False)
+		sqlData[sqlTable] = data
+		sqlColumns[sqlTable] = columns
+	cur.close()
+	db.close()
+	return sqlData,sqlColumns
 	
-def getSQLTableData(sqlCursor, tableName):
-	print("## RETRIEVING FROM SQL TABLE : %s ##\n"%tableName)
-	count = 0
-	sqlCursor.execute("SELECT count(*) FROM information_schema.columns WHERE table_name = '" + tableName + "'")
+def getSQLTableData(sqlCursor, tableName, toPrint):
+	print("## RETREIVING SQL TABLE %s ##\n"%(tableName))
+	sqlCursor.execute("SELECT column_name FROM information_schema.columns WHERE table_name = '" + tableName + "'")
+	columns = []
 	for row in sqlCursor.fetchall():
-		count = row[0]
+		columns.append(row[0])
 	sqlCursor.execute("SELECT * from " + tableName)
-	return count
-	
-def printSQLTableData(sqlCursor, tableName, numRows, numColumns, toPrint):
-	if (toPrint):
-		message = "## PRINTING"
-	else:
-		message = "## STORING"
-	print(message + " SQL TABLE %s WITH %d COLUMNS ##\n"%(tableName,numColumns))
-	rowCount = 0
-	allRows = False
-	if (numRows == 0):
-		allRows = True
+	data = []
 	for row in sqlCursor.fetchall():
-		rowCount = rowCount + 1
-		for col in range(0,numColumns):
+		rowData = {}
+		for col in range(0,len(columns)):
+			rowData[columns[col]] = row[col]
 			if (toPrint): 
 				print(row[col]),
+		data.append(rowData)
 		if (toPrint):
 			print
-		if ((not allRows) and (rowCount > numRows)):
-			break
+	return data,columns
 			
 ########################################################################################
 #	MONGODB FUNCTIONS
 ########################################################################################
 
 def connectToMongo():
-	print("## CONNECTING TO MONGO DATABASE ##\n")
+	print("## CONNECTING TO MONGO DATABASE %s ON PORT %d##\n"%(mongoHost,mongoPort))
 	client = pymongo.MongoClient(mongoHost, mongoPort)
-	
-def createNewMongoDB(client):
 	client.drop_database('f1_db')
 	db = client.f1_db
 	return db
 	
+def storeInMongoTableStatuses(mongoDB,tableName,sqlData,sqlColumns):
+	print("## STORING %s DATA TO MONGO DATABASE ##\n"%(tableName))
+	columns = sqlColumns[tableName]
+	for sqlRow in sqlData[tableName]:
+		mongoDB.statuses.insert({columns[1]:sqlRow[columns[1]]})
+		
+def storeInMongoTableDrivers(mongoDB,tableName,sqlData,sqlColumns):
+	print("## STORING %s DATA TO MONGO DATABASE ##\n"%(tableName))
+	columns = sqlColumns[tableName]
+	for sqlRow in sqlData[tableName]:
+		doc = {}
+		for col in range(1,len(columns)):
+			if (columns[col] == "dob"):
+				dob = sqlRow[columns[col]]
+				sqlRow[columns[col]] = datetime.datetime(dob.year,dob.month,dob.day)
+			if (isinstance(sqlRow[columns[col]],basestring)):
+				print ord(sqlRow[columns[col]][1])
+				str = unicode(sqlRow[columns[col]],"utf-8")
+				sqlRow[columns[col]] = str
+			doc[columns[col]] = sqlRow[columns[col]]
+		mongoDB.statuses.insert(doc)		
 			
 ########################################################################################
 #	PYTHON CONFIGURATION
