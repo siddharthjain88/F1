0cec6207545a8aa8b4bf48043dd03dceddf896c7
Siddharth Jain <sjain@guidewire.com>
2014-12-08 16:08:35 -0800

    4 more tables

--- src/sql_to_mongo.py
+++ src/sql_to_mongo.py
@@ -49,7 +49,7 @@ def main():
 ########################################################################################
 	
 def retrieveSQLData():
-	print("## CONNECTING TO SQL DATABASE %s ON HOST %s##\n"%(sqlDB,sqlHost))
+	print("## CONNECTING TO SQL DATABASE %s ON HOST %s ##\n"%(sqlDB,sqlHost))
 	db = MySQLdb.connect(host=sqlHost, user=sqlUser, passwd=sqlPassword, db=sqlDB)
 	cur = db.cursor()
 	sqlTables = allSQLTableNames
@@ -87,7 +87,7 @@ def getSQLTableData(sqlCursor, tableName, toPrint):
 ########################################################################################
 
 def storeInMongo(sqlData,sqlColumns):
-	print("## CONNECTING TO MONGO DATABASE %s ON PORT %d##\n"%(mongoHost,mongoPort))
+	print("## CONNECTING TO MONGO DATABASE %s ON PORT %d ##\n"%(mongoHost,mongoPort))
 	client = pymongo.MongoClient(mongoHost, mongoPort)
 	client.drop_database('f1_db')
 	db = client.f1_db
@@ -97,6 +97,10 @@ def storeInMongo(sqlData,sqlColumns):
 	storeInMongoTableConstructors(db,"constructors",sqlData,sqlColumns)
 	storeInMongoTableSeasons(db,"seasons",sqlData,sqlColumns)
 	storeInMongoTableRaces(db,"races",sqlData,sqlColumns)
+	storeInMongoTableResults(db,"results",sqlData,sqlColumns)
+	storeInMongoTableDriversStandings(db,"driverStandings",sqlData,sqlColumns)
+	storeInMongoTableConstructorResults(db,"constructorResults",sqlData,sqlColumns)
+	storeInMongoTableConstructorStandings(db,"constructorStandings",sqlData,sqlColumns)
 	client.close()
 	
 def storeInMongoTableStatuses(mongoDB,tableName,sqlData,sqlColumns):
@@ -110,11 +114,14 @@ def storeInMongoTableDrivers(mongoDB,tableName,sqlData,sqlColumns):
 	columns = sqlColumns[tableName]
 	for sqlRow in sqlData[tableName]:
 		doc = {}
-		for col in range(1,len(columns)):
+		for col in range(0,len(columns)):
 			if (columns[col] == "dob"):
 				dob = sqlRow[columns[col]]
 				if (dob != None):
 					sqlRow[columns[col]] = datetime.datetime(dob.year,dob.month,dob.day)
+			if (columns[col] == "driverId"):
+				if (sqlRow[columns[col]] != None):
+					sqlRow[columns[col]] = int(sqlRow[columns[col]])
 			if (isinstance(sqlRow[columns[col]],basestring)):
 				str = sqlRow[columns[col]].decode('iso-8859-1').encode('utf8')
 				sqlRow[columns[col]] = str
@@ -152,7 +159,10 @@ def storeInMongoTableConstructors(mongoDB,tableName,sqlData,sqlColumns):
 	columns = sqlColumns[tableName]
 	for sqlRow in sqlData[tableName]:
 		doc = {}
-		for col in range(1,len(columns)):
+		for col in range(0,len(columns)):
+			if (columns[col] == "constructorId"):
+				if (sqlRow[columns[col]] != None):
+					sqlRow[columns[col]] = int(sqlRow[columns[col]])
 			if (isinstance(sqlRow[columns[col]],basestring)):
 				str = sqlRow[columns[col]].decode('iso-8859-1').encode('utf8')
 				sqlRow[columns[col]] = str
@@ -190,7 +200,7 @@ def storeInMongoTableRaces(mongoDB,tableName,sqlData,sqlColumns):
 					data = int(data)
 			if (columns[col] == "year"):
 				column = "season"
-				data = mongoDB.seasons.find_one({columns[col]: sqlRow[columns[col]]})["_id"]
+				data = mongoDB.seasons.find_one({columns[col]: sqlRow[columns[col]]})
 			if (columns[col] == "circuitId"):
 				column = "circuit"
 				data = mongoDB.circuits.find_one({columns[col]: sqlRow[columns[col]]})
@@ -205,8 +215,136 @@ def storeInMongoTableRaces(mongoDB,tableName,sqlData,sqlColumns):
 				data = sqlRow[columns[col]].decode('iso-8859-1').encode('utf8')
 			if (column != "time"):
 				doc[column] = data
+			doc["drivers"] = []
+			doc["constructors"] = []
 		mongoDB.races.insert(doc)
-			
+		
+def storeInMongoTableResults(mongoDB, tableName, sqlData, sqlColumns):
+	print("## STORING %s DATA TO MONGO DATABASE ##"%(tableName))
+	columns = sqlColumns[tableName]
+	totalLength = len(sqlData[tableName])
+	count = 0
+	print("Completed %d of %d"%(count,totalLength))
+	for sqlRow in sqlData[tableName]:
+		count = count + 1
+		if (count % 1000 == 0):
+			print("Completed %d of %d"%(count,totalLength))
+		race = mongoDB.races.find_one({"raceId": sqlRow[columns[1]]})
+		driver = mongoDB.drivers.find_one({"driverId": sqlRow[columns[2]]})
+		for col in range(0,len(columns)):
+			column = columns[col]
+			data = sqlRow[columns[col]]
+			if (column == "number" or column == "grid" or column == "positionOrder" or column == "points" or column == "laps" or column == "fastestLap" or column == "rank"):
+				if (data != None):
+					data = int(data)
+				if (column == "points"):
+					column = "racePoints"
+				if (column == "rank"):
+					column = "fastestLapRank"
+				driver[column] = data
+			if (column == "positionText" or column == "time" or column == "fastestLapTime"):
+				if (data != None):
+					data = sqlRow[columns[col]].decode('iso-8859-1').encode('utf8')
+					driver[column] = data
+			if (column == "fastestLapSpeed"):
+				if (data != None):
+					data = float(data)
+					driver[column] = data
+			if (column == "milliseconds"):
+				if (data != None):
+					data = int(data)
+					driver[column] = data
+			if (column == "constructorId"):
+				data = mongoDB.constructors.find_one({column: data})
+				column = "constructor"
+				driver[column] = data
+			if (column == "status"):
+				data = mongoDB.statuses.find_one({column: data})
+				column = "status"
+				driver[column] = data
+		race["drivers"].append(driver)
+		mongoDB.races.update({'_id':race["_id"]}, {"$set": race}, upsert=False)
+	print
+	
+def storeInMongoTableDriversStandings(mongoDB, tableName, sqlData, sqlColumns):
+	print("## STORING %s DATA TO MONGO DATABASE ##"%(tableName))
+	columns = sqlColumns[tableName]
+	totalLength = len(sqlData[tableName])
+	count = 0
+	print("Completed %d of %d"%(count,totalLength))
+	for sqlRow in sqlData[tableName]:
+		count = count + 1
+		if (count % 1000 == 0):
+			print("Completed %d of %d"%(count,totalLength))
+		driverRec = mongoDB.races.find_one({"raceId": sqlRow[columns[1]], "drivers.driverId": sqlRow[columns[2]]},{"_id" : 0, "drivers": {"$elemMatch" : {"driverId":sqlRow[columns[2]]}}})
+		if (driverRec == None):
+			driver = mongoDB.drivers.find_one({"driverId": sqlRow[columns[2]]})
+		else:
+			driver = driverRec["drivers"][0]
+		mongoDB.races.update({"raceId": sqlRow[columns[1]]},{"$pull" :{"drivers":{"driverId":sqlRow[columns[2]]}}})
+		for col in range(0,len(columns)):
+			column = columns[col]
+			data = sqlRow[columns[col]]
+			if (column == "points" or column == "position" or column == "wins"):
+				if (data!= None):
+					driver[column] = int(data)
+		mongoDB.races.update({"raceId": sqlRow[columns[1]]},{"$push" :{"drivers":driver}})
+	print
+	
+def storeInMongoTableConstructorResults(mongoDB, tableName, sqlData, sqlColumns):
+	print("## STORING %s DATA TO MONGO DATABASE ##"%(tableName))
+	columns = sqlColumns[tableName]
+	totalLength = len(sqlData[tableName])
+	count = 0
+	print("Completed %d of %d"%(count,totalLength))
+	for sqlRow in sqlData[tableName]:
+		count = count + 1
+		if (count % 1000 == 0):
+			print("Completed %d of %d"%(count,totalLength))
+		race = mongoDB.races.find_one({"raceId": sqlRow[columns[1]]})
+		constructor = mongoDB.constructors.find_one({"constructorId": sqlRow[columns[2]]})
+		if (constructor == None):
+			continue
+		for col in range(0,len(columns)):
+			column = columns[col]
+			data = sqlRow[columns[col]]
+			if (column == "points"):
+				if (data != None):
+					data = int(data)
+				if (column == "points"):
+					column = "racePoints"
+				constructor[column] = data
+		race["constructors"].append(constructor)
+		mongoDB.races.update({'_id':race["_id"]}, {"$set": race}, upsert=False)
+	print
+		
+def storeInMongoTableConstructorStandings(mongoDB, tableName, sqlData, sqlColumns):
+	print("## STORING %s DATA TO MONGO DATABASE ##"%(tableName))
+	columns = sqlColumns[tableName]
+	totalLength = len(sqlData[tableName])
+	count = 0
+	print("Completed %d of %d"%(count,totalLength))
+	for sqlRow in sqlData[tableName]:
+		count = count + 1
+		if (count % 1000 == 0):
+			print("Completed %d of %d"%(count,totalLength))
+		constructorRec = mongoDB.races.find_one({"raceId": sqlRow[columns[1]], "constructors.constructorId": sqlRow[columns[2]]},{"_id" : 0, "constructors": {"$elemMatch" : {"constructorId":sqlRow[columns[2]]}}})
+		if (constructorRec == None):
+			constructor = mongoDB.constructors.find_one({"constructorId": sqlRow[columns[2]]})
+		else:
+			constructor = constructorRec["constructors"][0]
+		if (constructor == None):
+			continue
+		mongoDB.races.update({"raceId": sqlRow[columns[1]]},{"$pull" :{"constructors":{"constructorId":sqlRow[columns[2]]}}})
+		for col in range(0,len(columns)):
+			column = columns[col]
+			data = sqlRow[columns[col]]
+			if (column == "points" or column == "position" or column == "wins"):
+				if (data!= None):
+					constructor[column] = int(data)
+		mongoDB.races.update({"raceId": sqlRow[columns[1]]},{"$push" :{"constructors":constructor}})
+	print
+		
 ########################################################################################
 #	PYTHON CONFIGURATION
 ########################################################################################
---
+++ tools/start_mongo_client.bat
@@ -0,0 +1,5 @@
+@ECHO off
+ECHO ## STARTING MONGO CLIENT ##
+cd C:\Tools\mongodb\bin
+mongo
+PAUSE
